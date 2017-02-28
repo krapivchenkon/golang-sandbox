46fef460249cb1b4450310782908ad321295dfb7
Nikolai Krapivchenko <krapivchenkon@gmail.com>
2017-02-28 05:26:57 +0100

    custom types comparision

--- custom-types.go
+++ custom-types.go
@@ -1,11 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "fmt"
+
+type ctx1 int
+type ctx2 int
+
+const (
+	key1 ctx1 = 0
+	key2 ctx2 = 0
 )
 
+// CustomTypesComp Go doesn't perform automatic type casts when comparing values
+// even if custom types derived from the same type.
+// thus its safe to use your custom type to safely put your values in context package
+func CustomTypesComp(a, b interface{}) {
+	fmt.Println("Custom types compare:")
+	fmt.Println("a == b = ", a == b)
+	// Now check if it will be different keys in map.
+
+	d := make(map[interface{}]string)
+	d[key1] = "key1"
+	d[key2] = "key2"
+	fmt.Println("map: ", d)
+	fmt.Println("key1=", d[key1], "; key2=", d[key2])
+	fmt.Println("=========")
+	// Output:
+	// Custom types compare:
+	// a == b =  false
+	// map:  map[0:key1 0:key2]
+	// key1= key1 ; key2= key2
+}
+
 func main() {
+	CustomTypesComp(key1, key2)
 
 	aCar := car{vehicle{5, 2}, red}
 	fmt.Println(aCar)
@@ -35,17 +62,16 @@ const (
 	black Color = "BLACK-COLOR"
 )
 
-
 // Using custom types
-type customslice []string
-
-realslice := []string(customslice{"one", "two"})    // convert from custom type to a basic type
-cs customslice = customslice([]string{"one", "two"}) // convert from a basic type to a custom type
-
-//Using custom types2
-
-type Timestamp time.Time
-
-stmp:=Timestamp(time.Now())
-
-rubyDate:=time.Time(stmp).Format(time.RubyDate)
+// type customslice []string
+//
+// realslice := []string(customslice{"one", "two"})    // convert from custom type to a basic type
+// cs customslice = customslice([]string{"one", "two"}) // convert from a basic type to a custom type
+//
+// //Using custom types2
+//
+// type Timestamp time.Time
+//
+// stmp:=Timestamp(time.Now())
+//
+// rubyDate:=time.Time(stmp).Format(time.RubyDate)
